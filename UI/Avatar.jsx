ec1845d5df4433413766f8e8e203304d77c29b95
1amSumit <sj779619@gmail.com>
2024-02-06 18:41:28 +0530

    profile pic div changed to button

--- UI/Avatar.jsx
+++ UI/Avatar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function Avatar({ image }) {
   return (
-    <div className="w-[8rem] h-[8rem] rounded-full">
+    <button className="w-[8rem] h-[8rem] rounded-full">
       <img
         src={
           "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
@@ -9,6 +9,6 @@ export default function Avatar({ image }) {
         alt="user profile image"
         className="w-[100%] h-[100%] rounded-full"
       />
-    </div>
+    </button>
   );
 }
