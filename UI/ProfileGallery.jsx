09f918bedd6384ccc65fbf31c07997dcd7d4b590
1amSumit <sj779619@gmail.com>
2024-02-29 17:17:36 +0530

    gallery done

--- UI/ProfileGallery.jsx
+++ UI/ProfileGallery.jsx
@@ -3,7 +3,7 @@ import PostPreview from "./PostPreview";
 /* eslint-disable react/prop-types */
 export default function ProfileGallery({ posts }) {
   return (
-    <div className="grid grid-cols-3 gap-3 justify-between h-[60vh] overflow-y-scroll no-scrollbar">
+    <div className="grid mt-[2rem] px-[1rem] grid-cols-3 gap-3 justify-between h-[60vh] overflow-y-scroll no-scrollbar">
       {posts.userPosts.map((post) => (
         <div key={post}>
           <PostPreview imageUrl={post.post} />
