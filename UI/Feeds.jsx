a9ad3be334650660c06a0f66641cfe5b7124dbe8
1amSumit <sj779619@gmail.com>
2024-03-02 00:14:57 +0530

    getting feeds on loggedin state

--- UI/Feeds.jsx
+++ UI/Feeds.jsx
@@ -1,23 +1,7 @@
+/* eslint-disable react/prop-types */
 import Feed from "./Feed";
-import { getAllPosts } from "../services/getFeeds";
-import { useQuery } from "@tanstack/react-query";
 
-export default function Feeds() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["posts"],
-    queryFn: getAllPosts,
-  });
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error loading feeds</div>;
-  }
-  if (data.data.posts.length === 0) {
-    return <p>No posts to display.</p>;
-  }
-
+export default function Feeds({ data }) {
   return (
     <main className="flex flex-col justify-center gap-[2rem] sm:px-[4rem] sm:py-[2rem]  px-[1.4rem] py-[0.6rem]">
       {data.data.posts.map((post) => (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import Feeds from "../../UI/Feeds";
 import StatusBar from "../../UI/StatusBar";
+import { getAllPosts } from "../../services/getFeeds";
+import { getCurrentLoggedInUser } from "../../utils/getUserToken";
 
 export default function Home() {
+  const loggedInUser = getCurrentLoggedInUser();
+  const { isLoading, data } = useQuery({
+    queryKey: ["feeds", loggedInUser],
+    queryFn: getAllPosts,
+  });
+
   return (
     <div className="flex flex-col">
       <StatusBar />
 
       <main className="h-[80vh] overflow-y-auto no-scrollbar ">
-        <Feeds />
+        {isLoading ? <p>Loading...</p> : <Feeds data={data} />}
       </main>
     </div>
   );
