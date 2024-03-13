2dd821f5e0763efbe2a60d3b0a496d0040a97151
1amSumit <sj779619@gmail.com>
2024-03-13 12:52:33 +0530

    messge side starting

--- UI/RightMessage.jsx
+++ UI/RightMessage.jsx
@@ -1,3 +1,12 @@
-export default function RightMessage() {
-  return <div>RightMessage</div>;
+/* eslint-disable react/prop-types */
+export default function RightMessage({ recieverUserName }) {
+  return (
+    <div className="flex flex-col">
+      <div className="h-[10vh] bg-red-500">
+        <h2>{recieverUserName}</h2>
+      </div>
+      <div className="h-[80vh] bg-black">second</div>
+      <div className="h-[10vh] bg-green-500">third</div>
+    </div>
+  );
 }
--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -2,7 +2,7 @@ import LeftMessages from "../../UI/LeftMessages";
 import { getCurrentLoggedInUser } from "../../utils/getUserToken";
 import { useQuery } from "@tanstack/react-query";
 import { getPofileDetails } from "../../services/getProfileDetails";
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 
 export default function Messages() {
   const user = getCurrentLoggedInUser();
@@ -10,6 +10,8 @@ export default function Messages() {
     queryKey: ["loggedInUser"],
     queryFn: () => getPofileDetails(user),
   });
+
+  const params = useParams();
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -20,7 +22,15 @@ export default function Messages() {
         <LeftMessages loggedInUser={loggedInUser} />
       </div>
       <div className="col-span-2 ">
-        <Outlet />
+        {!params.messageId ? (
+          <div className="h-[100vh] flex justify-center items-center">
+            <p>Send photos and messages to frineds on in group.</p>
+          </div>
+        ) : (
+          <div className="h-[100vh]">
+            <Outlet />
+          </div>
+        )}
       </div>
     </div>
   );
