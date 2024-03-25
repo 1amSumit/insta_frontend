da8bba85fe0bcbe3b166eb13d6375bf50b088989
1amSumit <sj779619@gmail.com>
2024-03-25 18:26:19 +0530

    user AppError chnagred

--- UI/RightMessage.jsx
+++ UI/RightMessage.jsx
@@ -32,7 +32,7 @@ export default function RightMessage() {
   const { data: messages, isLoading: messageLoading } = useQuery({
     queryKey: [messageId, "mesages"],
     queryFn: () => getMessage(messageId),
-    // staleTime: 500,
+    staleTime: 500,
     refetchInterval: 500,
   });
 
