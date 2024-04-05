45cc9911abd76c715d8c0ef7f2eb281f8a5bfe19
1amSumit <sj779619@gmail.com>
2024-04-05 17:56:48 +0530

    loading skleton added

--- UI/LeftMessages.jsx
+++ UI/LeftMessages.jsx
@@ -4,6 +4,9 @@ import { useQuery } from "@tanstack/react-query";
 import { getCurrentLoggedInUser } from "../utils/getUserToken";
 import { getInbox } from "../services/getInbox";
 
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 /* eslint-disable react/prop-types */
 export default function LeftMessages() {
   const loggedInUser = getCurrentLoggedInUser();
@@ -12,8 +15,36 @@ export default function LeftMessages() {
     queryKey: ["inbox"],
     queryFn: () => getInbox(),
   });
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="px-4 pt-6">
+        <h2 className="text-2xl font-salsa">
+          <Skeleton width={150} height={20} />
+        </h2>
+        <div className="upper flex pt-8 flex-row justify-between px-[1rem]">
+          <button className="text-sm">
+            <Skeleton width={80} height={20} />
+          </button>
+          <button className="text-sm">
+            <Skeleton width={80} height={20} />
+          </button>
+        </div>
+        <div className="lower pt-5 flex flex-col gap-2">
+          <NavLink>
+            <div className="flex shadow-lg flex-row hover:bg-gray-300 duration-300 rounded-md gap-4 p-2 border-[1px] border-gray-200">
+              <div className="w-[3rem] h-[3rem] rounded-full items-center flex">
+                <Skeleton circle width={48} height={48} />
+              </div>
+              <div>
+                <Skeleton width={100} />
+                <Skeleton count={1} width={150} />
+              </div>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -23,7 +54,7 @@ export default function LeftMessages() {
         <button className="text-sm">Messages</button>
         <button className="text-sm">Requests</button>
       </div>
-      <div className="lower pt-5 flex flex-col gap-2">
+      <div className="lower  pt-5 flex flex-col gap-2">
         {data.inboxArr.map((inboxItem) => (
           <NavLink key={inboxItem.id} to={`/direct/t/${inboxItem.id}`}>
             <MessageProfile
--- UI/MessageProfile.jsx
+++ UI/MessageProfile.jsx
@@ -2,6 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { getUserById } from "../services/getUserByUSerId";
 import { useParams } from "react-router-dom";
 
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 /* eslint-disable react/prop-types */
 export default function MessageProfile({ userId, lastMessage }) {
   const messageId = useParams();
@@ -11,16 +14,50 @@ export default function MessageProfile({ userId, lastMessage }) {
     queryKey: [userId],
     queryFn: () => getUserById(userId),
   });
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div
+        className={`flex shadow-lg flex-row hover:bg-gray-300 duration-300 rounded-md gap-4 p-2 border-[1px] border-gray-200 ${
+          messageId.messageId === userId ? "bg-gray-300 text-gray-900" : ""
+        }`}
+      >
+        <div className="w-[3rem] h-[3rem] rounded-full items-center flex">
+          {data ? (
+            <img
+              className="rounded-full w-[100%] h-[100%]"
+              src={data.user.profilePic}
+              alt={data.user.username}
+            />
+          ) : (
+            <Skeleton circle width={48} height={48} />
+          )}
+        </div>
+        <div>
+          {data ? (
+            <p className="text-sm font-semibold">{data.user.username}</p>
+          ) : (
+            <Skeleton width={100} />
+          )}
+          {data && !isImage ? (
+            <p className="text-sm text-gray-700">{lastMessage.slice(0, 30)}</p>
+          ) : (
+            <Skeleton count={2} width={150} />
+          )}
+          {data && isImage ? (
+            <p className="text-sm text-gray-700">shared an attachment</p>
+          ) : null}
+        </div>
+      </div>
+    );
   }
 
   const isImage = lastMessage.endsWith(".png");
 
   return (
     <div
-      className={`flex flex-row hover:bg-gray-300 duration-300 rounded-md gap-4 p-2 border-[1px] border-gray-200 ${
-        messageId.messageId === userId ? "bg-gray-300" : ""
+      className={`flex shadow-lg flex-row  hover:bg-gray-300 duration-300 rounded-md gap-4 p-2 border-[1px] border-gray-200 ${
+        messageId.messageId === userId ? "bg-gray-300 text-gray-900" : ""
       }`}
     >
       <div className="w-[3rem] h-[3rem] rounded-full items-center flex">
@@ -31,7 +68,7 @@ export default function MessageProfile({ userId, lastMessage }) {
         />
       </div>
       <div>
-        <p className="text-sm">{data.user.username}</p>
+        <p className="text-sm font-semibold">{data.user.username}</p>
         {!isImage && (
           <p className="text-sm text-gray-700">{lastMessage.slice(0, 30)}</p>
         )}
