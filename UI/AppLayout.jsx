9944592f92a52e48ad23c4b0f4596870d82e0f09
1amSumit <sj779619@gmail.com>
2024-05-01 00:10:57 +0530

    bottom tab navigation

--- UI/AppLayout.jsx
+++ UI/AppLayout.jsx
@@ -3,20 +3,25 @@ import Sidebar from "./Sidebar";
 import Suggetions from "./Suggetions";
 import { getAuthToken } from "../utils/getUserToken";
 import { useEffect } from "react";
+import MobileBottomTabBar from "./MobileBottomTabBar";
 
 export default function AppLayout() {
   const navigate = useNavigate();
   const userToken = getAuthToken();
   const url = window.location.pathname;
+
   useEffect(() => {
     if (userToken === null) {
       navigate("/login");
     }
   }, [userToken, navigate]);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-5 h-screen">
+    <div className="grid relative grid-cols-1 sm:grid-cols-5 h-screen">
       <div className="hidden sm:block bg-stone-900 text-gray-100">
         <Sidebar />
+      </div>
+      <div className="md:hidden absolute bottom-0 left-0 right-0 h-[4rem] z-40">
+        <MobileBottomTabBar />
       </div>
       <main
         className={` ${
---
+++ UI/MobileBottomTabBar.jsx
@@ -0,0 +1,5 @@
+function MobileBottomTabBar() {
+  return <div className="bg-stone-700 text-gray-100 h-full">hallo</div>;
+}
+
+export default MobileBottomTabBar;
