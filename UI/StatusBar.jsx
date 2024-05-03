f2d3a49bbcd8577e96ebe1e9b614e1d59f996e8a
1amSumit <sj779619@gmail.com>
2024-05-03 17:47:28 +0530

    status implemented

--- UI/StatusBar.jsx
+++ UI/StatusBar.jsx
@@ -1,13 +1,36 @@
 import StatusItem from "./StatusItem";
 
+const statusBarData = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1714307410102-8b500822b5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1714307410102-8b500822b5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1714307410102-8b500822b5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1714307410102-8b500822b5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
 export default function StatusBar() {
   return (
     <aside className="flex flex-row border-b-[1px] border-gray-200 overflow-x-auto px-2 py-5 no-scrollbar">
       <div className="flex gap-2 items-center">
-        <StatusItem size={"medium"} />
-        <StatusItem size={"medium"} />
-        <StatusItem size={"medium"} />
-        <StatusItem size={"medium"} />
+        {statusBarData.map((data) => (
+          <StatusItem
+            key={data.id}
+            size={"medium"}
+            content={data.img}
+            profilePic={""}
+          />
+        ))}
       </div>
     </aside>
   );
--- UI/StatusItem.jsx
+++ UI/StatusItem.jsx
@@ -1,27 +1,76 @@
+import { useEffect, useState } from "react";
+import Modal from "./Modal";
+
 // eslint-disable-next-line react/prop-types
-export default function StatusItem({ size, profilePic }) {
-  const onClickhandler = () => {};
+export default function StatusItem({ size, profilePic, content }) {
+  const [modelOpen, setModelOpen] = useState(false);
+  const [timerWidth, setTimerWidth] = useState(0);
+
+  useEffect(() => {
+    if (modelOpen) {
+      const timer = setTimeout(() => {
+        setModelOpen(false);
+        setTimerWidth(0);
+      }, 5000);
+
+      const interval = setInterval(() => {
+        setTimerWidth((prevWidth) => prevWidth + (100 / 5000) * 100);
+      }, 100);
+
+      return () => {
+        clearTimeout(timer);
+        clearInterval(interval);
+      };
+    }
+  }, [modelOpen]);
+
+  const onClickhandler = () => {
+    setModelOpen(true);
+    setTimerWidth(0);
+  };
+
+  const onClose = () => {
+    setModelOpen(false);
+    setTimerWidth(0);
+  };
 
   const variants = {
-    smallest: "w-[1.5rem] h-[]1.5rem] rounded-full",
+    smallest: "w-[1.5rem] h-[1.5rem] rounded-full",
     small: "w-[3rem] h-[3rem] rounded-full",
     medium: "w-[4rem] h-[4rem] rounded-full",
     large: "w-[6rem] h-[6rem] rounded-full",
   };
 
   return (
-    <button onClick={() => onClickhandler()}>
-      <figure className={variants[size]}>
-        <img
-          src={
-            profilePic
-              ? profilePic
-              : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-          }
-          alt="profile picture"
-          className="w-[100%] h-[100%] border-2 border-red-300 rounded-full bg-contain"
-        />
-      </figure>
-    </button>
+    <>
+      <button onClick={onClickhandler}>
+        <figure className={variants[size]}>
+          <img
+            src={
+              profilePic
+                ? profilePic
+                : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+            }
+            alt="profile picture"
+            className="w-[100%] h-[100%] border-2 border-red-300 rounded-full bg-contain"
+          />
+        </figure>
+      </button>
+      <Modal isOpen={modelOpen} onClose={onClose}>
+        <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+          <div className="w-[20%] relative h-[80%] rounded-lg text-white">
+            <img
+              src={content}
+              alt="status"
+              className="w-full h-full rounded-lg"
+            />
+            <div
+              className="timer absolute h-[0.3rem] rounded-lg top-1 left-0 right-0 bg-gray-500"
+              style={{ width: `${timerWidth}%` }}
+            ></div>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 }
